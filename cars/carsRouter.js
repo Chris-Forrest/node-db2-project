bb9fe2bd6e198f2e7aa9c8ca7f9801dbd486b023
Chris Forrest <redrider_cr@yahoo.com>
2020-08-21 22:06:30 -0400

    made the get and post requests both work mvp met will try stretch

--- cars/carsRouter.js
+++ cars/carsRouter.js
@@ -4,6 +4,22 @@ const db = require("../data/config")
 
 const router = express.Router()
 
+router.get("/cars", async (req,res,next) => {
+    try{
+        res.json(await db("cars"))
+    }catch(err){
+        next(err)
+    }
+});
 
+router.post("/cars", async (req,res,next) => {
+    try{
+        const [id] = await db("cars").insert(req.body)
+        const newCar = await db("cars").where({ id }).first()
+        res.status(201).json(newCar)
+    }catch(err){
+        next(err)
+    }
+})
 
 module.exports = router
