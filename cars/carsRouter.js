05654de21332fcefdc3e9d284f7bc7e68b6b3f3a
Chris Forrest <redrider_cr@yahoo.com>
2020-08-25 13:11:30 -0400

    stretch edit car and delete car end points working

--- cars/carsRouter.js
+++ cars/carsRouter.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const knex = require("knex")
-const db = require("../data/config")
+const db = require("../data/config");
+const { OPEN_READWRITE } = require("sqlite3");
 
 const router = express.Router()
 
@@ -17,6 +18,47 @@ router.post("/cars", async (req,res,next) => {
         const [id] = await db("cars").insert(req.body)
         const newCar = await db("cars").where({ id }).first()
         res.status(201).json(newCar)
+    }catch(err){
+        next(err)
+    }
+});
+
+router.get("/cars/:id", async (req,res,next) => {
+    try{
+        const { id } = req.params
+        const car = await db("cars").where({ id }).first()
+        res.json(car)
+    }catch(err){
+        next(err)
+    }
+});
+
+router.put("/cars/:id", async (req,res,next) => {
+    try{
+        await db("cars").update({
+            VIN: req.body.VIN,
+            make: req.body.make,
+            model: req.body.model,
+            mileage: req.body.mileage,
+            transmission: req.body.transmission,
+            title: req.body.title
+        })
+        .where("id", req.params.id)
+        const car = await db("cars")
+        .where("id", req.params.id)
+        .first()
+        res.json(car)
+    }catch(err){
+        next(err)
+    }
+});
+
+router.delete("/cars/:id", async (req,res,next) => {
+    try{
+        await db("cars")
+        .where("id", req.params.id)
+        .del()
+        res.status(204).end()
     }catch(err){
         next(err)
     }
